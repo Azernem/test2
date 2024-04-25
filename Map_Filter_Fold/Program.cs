7447bd949f876eeb756c8bfc24ec7aaec8d48372
Azernem <nematmusaev047.@gmail.com>
2024-04-26 02:39:33 +0300

    generices files

---
+++ Map_Filter_Fold/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Functions
+{
+    public delegate Tres F_for_Map<T, Tres>(T value);
+    public List<Tres> Map<T, Tres>(List<T> list_start, F_for_Map<T, Tres> fun)
+    {
+        List<Tres> result = [];
+        for (int i = 0; i<list_start.Count; ++i)
+        {
+            result.Add(fun(list_start[i]));
+        }
+        return result;
+    } 
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var _Functions = new Functions();
+        var list = new List<int> {1, 2, 3};
+        Console.WriteLine(_Functions.Map(list, x=>x*2)[1]);
+    }
+}
